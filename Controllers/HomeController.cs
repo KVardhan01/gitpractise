8748d467dc6687795d25d116f5d7b07271eaa613
vardhan <kokkiligaddavardhan0000@gmail.com>
2024-06-05 11:21:50 +0530

    first

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok("vardhan");
+        }
+    }
+}
